c325c46bb64e5e3f594ccb535c8925b1c998f06d
Augie Bren <Augie>
2022-03-22 23:19:18 -0500

    learn the function

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+
+function newImage() {
+    let img = document.createElement('img')
+    img.src = 'assets/green-character.gif'
+    img.style.position = 'fixed'
+    img.style.left = '100px'
+    img.style.bottom = '100px'
+    document.body.append(img)
+
+    let img1 = document.createElement('img')
+    img1.src = 'assets/pine-tree.png'
+    img1.style.position = 'fixed'
+    img1.style.left = '350px'
+    img1.style.bottom = '175px'
+    document.body.append(img1)
+}
+newImage()
